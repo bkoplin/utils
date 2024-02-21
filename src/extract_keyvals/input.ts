2248cee82843d80c846eeda9c1be4199acd8665a
Ben Koplin <benjaminkoplin@outlook.com>
2024-02-21 16:07:22 -0600

    feat: group values with duplicate keys

--- src/extract_keyvals/input.ts
+++ src/extract_keyvals/input.ts
@@ -1,23 +1,35 @@
-// plv8ify generate --write-bundler-output
+// pnpm plv8ify generate --input-file="./src/extract_keyvals/input.ts" --output-folder="./src/extract_keyvals"
 // See ./types.ts for type conversions
 
 // const word = maybe('"').before(oneOrMore(anyOf(wordChar, '_', '-', digit)).grouped().after(maybe('"')))
 // const keyVal = createRegExp(word.as('key'), '=', word.as('value'))
-import { attempt, isError } from 'lodash-es'
+import { attempt, groupBy, isEmpty, isError, isNil, trim, uniq } from 'lodash-es'
 
 export function extract_keyvals(input: string) {
-  const regex = /(?<key>(?:\w|_|-|\d)+)"?=(?<value>(?:(?:\w|_|-|\d)+)|(?:\[[^\]]+\])|(?:"[^"]+"))/g
+  const regex = /(?<key>(?:\w|_|-|\d)+)"?=(?<value>(?:(?:[A-Z][a-z]{2} [A-Z][a-z]{2} \d{2} \d{2}:\d{2}:\d{2} [A-Z]{3} \d{4})|(?:\w|_|-|\d)+)|(?:\[[^\]]+\])|(?:"[^"]+"))/g
   const server = /\/\d+\.\d+\.\d+\.\d+\:\d+/
   const equalsMatches = input.matchAll(regex)
   const serverMatches = server.exec(input)
-  const obj = Array.from(equalsMatches).reduce((acc, match) => {
-    if (match.groups?.key && match.groups?.value)
-      return { ...acc, [match.groups.key]: isError(attempt(JSON.parse, match.groups.value)) ? match.groups.value : JSON.parse(match.groups.value) }
-    else return acc
-  }, {})
-  if (serverMatches !== null)
-    return { server: serverMatches[0], ...obj }
-  else if (Object.keys(obj).length)
-    return obj
-  else return null
+  const groups = Array.from(equalsMatches).reduce((acc, match) => {
+    if (typeof match.groups !== 'undefined') {
+      const { key, value: origValue } = match.groups
+      const value = isError(attempt(JSON.parse, trim(origValue, ' "'))) ? trim(origValue, ' "') : JSON.parse(trim(origValue, ' "'))
+      if (key && typeof value !== 'undefined')
+        acc.push({ key, value })
+    }
+    return acc
+  }, [] as { key: string, value: string }[])
+  const obj = groupBy(groups, 'key')
+  for (const k in obj) {
+    obj[k] = uniq(obj[k].map(({ key, value }) => value))
+    if (obj[k].length === 1)
+      obj[k] = obj[k][0]
+  }
+
+  if (isNil(serverMatches)) {
+    if (isEmpty(obj))
+      return null
+    else return obj
+  }
+  else { return { server: serverMatches[0], ...obj } }
 }
