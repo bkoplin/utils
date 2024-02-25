a6e697287911d0aff44ddd4a8a5f856ed315c14e
Ben Koplin <benjaminkoplin@outlook.com>
2024-02-25 05:11:41 -0600

    feat: use `type-level-regexp` library

--- src/extract_keyvals/input.ts
+++ src/extract_keyvals/input.ts
@@ -1,24 +1,29 @@
 // pnpm plv8ify generate --input-file="./src/extract_keyvals/input.ts" --output-folder="./src/extract_keyvals"
-// See ./types.ts for type conversions
+import { attempt, groupBy, isEmpty, isError, isNil, snakeCase, trim, uniq } from 'lodash-es'
+import type { ParseRegExp } from 'type-level-regexp'
+import { createRegExp, spreadRegExpIterator } from 'type-level-regexp'
+import type { SnakeCase } from 'type-fest'
 
-// const word = maybe('"').before(oneOrMore(anyOf(wordChar, '_', '-', digit)).grouped().after(maybe('"')))
-// const keyVal = createRegExp(word.as('key'), '=', word.as('value'))
-import { attempt, groupBy, isEmpty, isError, isNil, trim, uniq } from 'lodash-es'
+type TestInputType = `[2021-10-04 17:56:55,374 UTC][INFO][AutoTriggerWorker][pool-1-thread-10][serial_number=A124750753 request_id=38859467 rules_version=2] Patch command created: PatchCommand{id=81603658, serialNumber=A124750753, type=VT_NN, startPage=163, endPage=175, status=UNSUBMITTED, analysisRequestId=38859467, priority=80, batchNumber=2, createdTimestamp=Mon Oct 04 17:56:55 UTC 2021, pcStartEcgPage=169, pcStartEcgOffset=90, pcEndEcgPage=170, pcEndEcgOffset=592, decisionThreshold=0.6} rpeak_request_list=true request_id=38859467` | `[2021-10-04 17:26:42.161 UTC] [INFO] [nioEventLoopGroup-3-4] ServerChallengeResponseHandler - [/174.245.252.233:45008] [A124750753] uptime=52, events=0, log_bytes=184, connection_reason=FIRST_CONNECTION, diagnostics=true, rpeak_pages=0` | `[2021-10-04 17:56:53.191 UTC] [INFO] [pool-13-thread-1] Logging - AUDIT[salesforce]: Updating device percentage leads on, percent_leads_on="100.00" attempted for device A124750753 | object="DEVICE" | status="SUCCESS" | error=""`
 
-export function extract_keyvals(input: string) {
-  const regex = /(?<key>(?:\w|_|-|\d)+)"?=(?<value>(?:(?:[A-Z][a-z]{2} [A-Z][a-z]{2} \d{2} \d{2}:\d{2}:\d{2} [A-Z]{3} \d{4})|(?:\w|_|-|\d)+)|(?:\[[^\]]+\])|(?:"[^"]+"))/g
-  const server = /(\/\d+\.\d+\.\d+\.\d+\:\d+)/
-  const equalsMatches = input.matchAll(regex)
-  const serverMatches = server.exec(input)?.[1]
-  const groups = Array.from(equalsMatches).reduce((acc, match) => {
-    if (typeof match.groups !== 'undefined') {
+export function extract_keyvals(input: TestInputType) {
+  const regex = createRegExp('\\b(?<key>\\w+)"?=(?<value>(?:\\w{3} \\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} \\w{3} \\d{4})|(?:"([^"]+)")|(?:\\[([^\\]]+)\\])|(?:\\w+))', ['g'])
+  const serverRegex = createRegExp('\\/(?<server>(?:\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+))' as const)
+  const logtimeRegex = createRegExp(`((?:\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2})\\W(?:\\d{3}) (?:[A-Z]{3}))` as const)
+  const serverMatch = input.match(serverRegex)
+  const server = serverMatch !== null ? serverMatch.groups.server : null
+  const inputMatches = input.matchAll(regex)
+  const logTime = input.match(logtimeRegex)?.[0]?.replace(createRegExp(','), '.') ?? null
+  const matches = spreadRegExpIterator(inputMatches)
+  const groups = matches.map((match) => {
+    if (!isNil(match) && typeof match.groups !== 'undefined') {
       const { key, value: origValue } = match.groups
       const value = isError(attempt(JSON.parse, trim(origValue, ' "'))) ? trim(origValue, ' "') : JSON.parse(trim(origValue, ' "'))
       if (key && typeof value !== 'undefined')
-        acc.push({ key, value })
+        return { key: snakeCase(key) as SnakeCase<typeof key>, value }
     }
-    return acc
-  }, [] as { key: string, value: string }[])
+    return { key: null, value: null }
+  })
   const obj = groupBy(groups, 'key')
   for (const k in obj) {
     obj[k] = uniq(obj[k].map(({ key, value }) => value))
@@ -26,10 +31,5 @@ export function extract_keyvals(input: string) {
       obj[k] = obj[k][0]
   }
 
-  if (isNil(serverMatches)) {
-    if (isEmpty(obj))
-      return null
-    else return obj
-  }
-  else { return { ...obj, server: serverMatches } }
+  return { ...obj, server, log_time: logTime }
 }
