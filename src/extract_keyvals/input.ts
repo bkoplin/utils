733d1a6ed5c87b5ac72436fff13e488127bb468c
Ben Koplin <benjaminkoplin@outlook.com>
2024-02-26 04:21:12 -0600

    feat: extract serial number into keyvals

--- src/extract_keyvals/input.ts
+++ src/extract_keyvals/input.ts
@@ -5,12 +5,11 @@ import { createRegExp, spreadRegExpIterator } from 'type-level-regexp'
 import type { SnakeCase } from 'type-fest'
 import balanced from 'balanced-match'
 
-type TestInputType = `[2021-10-04 17:56:55,374 UTC][INFO][AutoTriggerWorker][pool-1-thread-10][serial_number=A124750753 request_id=38859467 rules_version=2] Patch command created: PatchCommand{id=81603658, serialNumber=A124750753, type=VT_NN, startPage=163, endPage=175, status=UNSUBMITTED, analysisRequestId=38859467, priority=80, batchNumber=2, createdTimestamp=Mon Oct 04 17:56:55 UTC 2021, pcStartEcgPage=169, pcStartEcgOffset=90, pcEndEcgPage=170, pcEndEcgOffset=592, decisionThreshold=0.6} rpeak_request_list=true request_id=38859467` | `[2021-10-04 17:26:42.161 UTC] [INFO] [nioEventLoopGroup-3-4] ServerChallengeResponseHandler - [/174.245.252.233:45008] [A124750753] uptime=52, events=0, log_bytes=184, connection_reason=FIRST_CONNECTION, diagnostics=true, rpeak_pages=0` | `[2021-10-04 17:56:53.191 UTC] [INFO] [pool-13-thread-1] Logging - AUDIT[salesforce]: Updating device percentage leads on, percent_leads_on="100.00" attempted for device A124750753 | object="DEVICE" | status="SUCCESS" | error=""`
-
-export function extract_keyvals(input: TestInputType) {
+export function extract_keyvals(input: string) {
   const regex = createRegExp('\\b(?<key>\\w+)"?=(?<value>(?:\\w{3} \\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} \\w{3} \\d{4})|(?:"([^"]+)")|(?:\\[([^\\]]+)\\])|(?:\\w+))', ['g'])
   const serverRegex = createRegExp('\\/(?<server>(?:\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+))' as const)
   const serverMatch = input.match(serverRegex)
+  const serialNumber = input.match(/\[(A\d+)\]/)?.[1]
   const server = serverMatch !== null ? serverMatch.groups.server : null
   const inputMatches = input.matchAll(regex)
   const logTime = balanced('[', ']', input)!.body.replaceAll(',', '.')
@@ -31,5 +30,5 @@ export function extract_keyvals(input: TestInputType) {
       obj[k] = obj[k][0]
   }
 
-  return { ...obj, server, log_time: logTime }
+  return { serial_number: serialNumber, ...obj, server, log_time: logTime }
 }
